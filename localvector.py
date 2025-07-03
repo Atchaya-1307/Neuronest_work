2c025adcdc08727262d50849be60c90c431b97b8
AHALYA387 <ahalyagnanamurugan3@gmail.com>
2025-07-03 16:53:47 +0530

    first commit

--- localvector.py
+++ localvector.py
@@ -11,7 +11,6 @@ llm = ChatGroq(
     model="llama-3.1-8b-instant",
     temperature=0.0,
     max_retries=2,
-    api_key=os.environ.get("g***********"),
     # other params...
 )
 # 1️⃣ Create embedding model
